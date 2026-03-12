d21ed24050c3bcb83f85c7b51b98621e01c3e45c
Devjack404 <juansabarofek@gmail.com>
2026-03-12 18:25:34 +0700

    add : file bubble sorting versi 2

--- sms2/bubble-sort.cpp
+++ sms2/bubble-sort.cpp
@@ -2,55 +2,41 @@
 using namespace std;
 
 int main (){
-    int arr[] = {5, 3, 8, 1};
-    int n = 4;
+//     int arr[] = {64, 25, 12, 22, 11};
+// int n = 5;
+// ```
 
-    for(int i = 0; i < n; i++){
-        for ( int j = 0; j < n -1 -i; j++){
+// **Tugasmu:**
+
+// 1. Tulis bubble sort lengkap (outer + inner loop) di C++
+// 2. Print array **setelah setiap putaran** — contoh output yang diharapkan:
+// ```
+// Putaran 1: 25 12 22 11 64
+// Putaran 2: 12 22 11 25 64
+// ...
+
+    int arr[] = {64, 25, 12, 22, 11};
+    int  n = 5;
+    
+
+    for (int i = 0; i < n ; i++){
+        bool adaSwap = false;
+        for(int j = 0; j < n -1 -i ; j++){
             if(arr[j] > arr[j + 1]){
-                int sementera = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = sementera;
-            }
+                //tukar
+                int kos = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j+1] = kos;
+                adaSwap = true;
+            }       
         }
+        if(adaSwap == false) break;
+        cout << "putaran " << i+1 << " : ";
+        for (int j = 0; j < n; j++){
+            cout << arr[j] << " ";   
+        }
+        cout << endl;
     }
-    for (int i = 0; i < n;i++){
-        cout << arr[i] << " ";
-    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     int arr[] = {5, 3, 8, 1};
-//     int n = 4;
-
-//     for(int i = 0; i < n ;i++){
-//         for(int j = 0; j < n -1 -i ; j++){
-//             if(arr[j] > arr[j + 1]){
-//                 //tukar
-//                 int temp = arr[j];
-//                 arr[j] = arr[j + 1];
-//                 arr[j + 1] = temp;
-//             }
-//         }
-//     }
-
-//     for(int i = 0; i < n; i++){
-//         cout << arr[i] << " ";
-//     }
-
-
-//     return 0;
+    return 0;
 };
