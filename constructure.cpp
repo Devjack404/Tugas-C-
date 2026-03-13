16f28edc32bb593f93b6a46e3d84e95b0321f69e
Devjack404 <juansabarofek@gmail.com>
2026-03-13 23:48:33 +0700

    feat : info function untuk setiap karakter

--- constructure.cpp
+++ constructure.cpp
@@ -2,35 +2,61 @@
 #include <string>
 using namespace std;
 
+
+// latihan kecil
+// coba tambahkan:
+
+// atribut int level ke class knight
+// ubah constructor supaya menerima level juga
+// buat fungsi info() yang mencetak nama, health, dan level
+
 class Knight{
     public :
 
     string nama;
     int health;
+    int level;
+
 
     // Constructure
-    Knight(string n, int h){
+    Knight(string n, int h, int l){
         nama = n;
         health = h;
+        level = l;
 
     }
 
     void serang(){
         cout << nama << "Menyerang Musuh ..." << endl;
+        cout << endl; 
     }
+
+    void info(){ 
+        cout << "nama : "  << nama << endl;
+        cout << "Nyawa : " << health << endl;
+        cout  << "Level : " << level << endl;
+    }
+    
 };
 
 
 
 
 int main(){
-   Knight k1("Juan ", 100);
-   Knight k2("Mesak ", 90);
-   Knight k3("Udina ", 95);
+   Knight k1("Juan ", 100, 50);
+   Knight k2("Mesak ", 90, 45);
+   Knight k3("Udina ", 95, 48);
 
+   k1.info();
    k1.serang();
+
+   k2.info();
    k2.serang();
+
+   k3.info();
    k3.serang();
+
+
 
 
     return 0;
