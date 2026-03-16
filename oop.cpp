e77813eb95747ae28fd48d47d909b4e347c84a93
Devjack404 <juansabarofek@gmail.com>
2026-03-16 22:03:31 +0700

    add : function hitung Luas Persegi

--- oop.cpp
+++ oop.cpp
@@ -10,10 +10,14 @@ class Mahasiswa {
     string nama;
     string nim;
     int umur;
+
+    int Luas (int p, int l){
+        return p * l;
+    }
 };
 
 int main () {
-    Mahasiswa mhs1, mhs2;
+    Mahasiswa mhs1, mhs2, x;
 
     mhs1.nama ="Jack";
     mhs1.nim = "A11.2025";
@@ -22,8 +26,6 @@ int main () {
     mhs2.nama ="Budi";
     mhs2.nim = "A11.2024";
     mhs2.umur = 20;
-
-
     
     
     cout << "Data Mahasiswa 1" << endl; 
@@ -36,7 +38,12 @@ int main () {
     cout << "Nama : " << mhs2.nama << endl;
     cout << "Nim : " << mhs2.nim << endl;
     cout << "Umur : " << mhs2.umur << endl;
+    cout << endl;
 
+
+
+    cout << "Test Function" << endl;
+    cout << "Luas Persegi : " << x.Luas(3,4) << endl;
 
 
     return 0;
