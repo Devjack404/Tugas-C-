25cf3a251e2d2010034b78056b1d47a7d097c4cc
Devjack404 <juansabarofek@gmail.com>
2026-03-30 22:05:07 +0700

    add find max array

---
+++ sms2/FindMaxArray.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+int cariMaksimum(int arr[], int n){
+    int max = arr[0];
+    for(int i = 1; i < n ; i++){
+        if(arr[i] > max){
+           max = arr[i]; 
+        }
+    }
+
+    return max;
+};
+
+int main (){
+    int arr[5] = {5, 2, 9, 1, 7};
+    int n = 5;
+
+    cout << "Nilai maksimum pada array : " << cariMaksimum(arr, n) << endl;
+    return 0;
+}
---
+++ sms2/bubblesort2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main (){
+    int arr[] = {64, 34, 25, 12, 22};
+    int n = 5;
+
+    for (int a = 0; a < n ; a++){
+        for(int i = 0; i < n -1 -i ; i++){
+            if (arr[i] >  )
+            std:: cout << a << " = tes" << endl;
+        }
+    }
+
+
+    return 0;
+}
